78b41ec43d963e528784e3887736dacd262e59b5
demooALX <harlomedal@gmail.com>
2023-07-19 05:09:15 -0700

    new commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h#
+
+/**
+ * main - test functions that prints if integer is positive or negative
+ * Return 0
+ * */
+
+
+int main (void){ 
+	int i;
+	i=0;
+	void positive_or_negative(int i);
+
+	return(0);
+
+}
