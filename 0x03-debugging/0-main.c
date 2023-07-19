773d241aab19b743b3ae3915d1f4d6082afedd74
demooALX <harlomedal@gmail.com>
2023-07-19 05:54:04 -0700

    ihope it works

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,9 +3,13 @@
  * main - test functions that prints if integer is positive or negative
  * Return 0
  * */
-int main (void){ 
+
+int main (void)
+{ 
+
 	int i;
 	i=0;
 	positive_or_negative(i);
 	return(0);
+
 }
