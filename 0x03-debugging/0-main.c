b6cb57b3381728458d16357f6a07d6a1a53fca8f
demooALX <harlomedal@gmail.com>
2023-07-19 05:12:22 -0700

    west

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,11 @@
 #include "main.h#
-
 /**
  * main - test functions that prints if integer is positive or negative
  * Return 0
  * */
-
-
 int main (void){ 
 	int i;
 	i=0;
 	void positive_or_negative(int i);
-
 	return(0);
-
 }
