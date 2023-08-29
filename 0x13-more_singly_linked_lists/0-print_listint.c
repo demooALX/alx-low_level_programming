9ae2cb63534373cac6499a8c074eb204d3e13f19
demooALX <harlomedal@gmail.com>
2023-08-29 02:27:53 -0700

    Deadline meetup

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a linked list
+ * @h: linked list of type listint_t to print
+ *
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *p)
+{
+	size_t num = 0;
+
+	while (p)
+	{
+		printf("%d\n", p->n);
+		num++;
+		p = p->next;
+	}
+
+	return (num);
+}
+
