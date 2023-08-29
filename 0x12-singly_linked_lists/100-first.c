9b7e28b7547b14b063ed55f8969d6b3a6dfc19ca
demooALX <harlomedal@gmail.com>
2023-08-29 01:49:11 -0700

    ww

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,6 +7,6 @@ void first(void) __attribute__ ((constructor));
  */
 void first(void)
 {
-	printf("You're beat! and yet, you must allow,\\nI bore my house upon my back!\\n\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
 
